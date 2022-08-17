d3c6e4152b48ee52f3d0eee88d0d45307b67327a
Razvan Andrei Berendei <berendei.razvan@gmail.com>
2022-08-17 20:30:46 +0300

    added the user input + input verification for the path of the process and the interval of analysis

--- main.py
+++ main.py
@@ -4,18 +4,39 @@ import csv
 from time import sleep
 
 
+# declare process and interval variables and initialize them
+process = None
+interval = ""
 
-process = psutil.Process(subprocess.Popen("C:/Program Files/JetBrains/IntelliJ IDEA Community Edition 2022.2/bin/idea64.exe").pid)
-#process1 = psutil.Process(4)
-#print(process1)
+# initialize the user path
+user_path = str(input("Choose the process (enter the full path): ").strip())
+
+# initialize the analysis interval
+while interval == "":
+    try:
+        interval = int(input("Choose the interval of analysis (Seconds): "))
+    except ValueError:
+        print("Warning: Invalid value for the interval, please enter a number.")
+
+# try to get the process pid (if the process path is validated)
+while process is None:
+    try:
+        process = psutil.Process(subprocess.Popen(user_path.replace("\\", '/')).pid)
+    except FileNotFoundError:
+        print("Warning: process not found.")
+        user_path = str(input("Choose the process (enter the full path): ").strip())
+    except OSError:
+        print("Warning: OSError encountered.")
+        user_path = str(input("Choose the process (enter the full path): ").strip())
+
 
 while True:
     try:
+        print("Name: " + str(process.name()))
         print("Percentage of CPU drained by the process: " + str(process.cpu_percent()/psutil.cpu_count()))
         print("Number of handles: " + str(process.num_handles()))
-        print("Working Set: " + str(process.memory_info().wset) + ", Private Bytes: " + str(process.memory_info().private))
-        sleep(1)
+        print("Working Set: " + str(process.memory_info().wset) + "\n" + "Private Bytes: " + str(process.memory_info().private))
+        sleep(interval)
     except psutil.NoSuchProcess:
         print("Proccess exited.")
         break
-        
